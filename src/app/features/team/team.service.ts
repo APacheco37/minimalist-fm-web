67812fd215e2f0413e87570043f2cd7bdf5c16c4
Andres Pacheco <apacheco73@hotmail.com>
2020-09-15 20:16:25 -0300

    Add team service methods

--- src/app/features/team/team.service.ts
+++ src/app/features/team/team.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+import { Team } from './team';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamService {
 
-  constructor() { }
+  private apiUrl = environment.apiUrl;
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getTeam(id: number): Observable<Team> {
+    return this.http.get<Team>(this.apiUrl + `team/${id}`);
+  }
+
+  getTeams(): Observable<Team[]> {
+    return this.http.get<Team[]>(this.apiUrl + `team`);
+  }
+
+  addTeam(team: Team): Observable<Team> {
+    return this.http.post<Team>(this.apiUrl + `team/`, team);
+  }
+
+  updateTeam(team: Team): Observable<Team> {
+    return this.http.put<Team>(this.apiUrl + `team/${team.Id}`, team);
+  }
+
+  deleteTeam(id: number): Observable<Team> {
+    return this.http.delete<Team>(this.apiUrl + `team/${id}`);
+  }
 }
