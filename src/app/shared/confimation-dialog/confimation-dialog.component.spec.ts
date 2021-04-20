2c9aeb872d414fc0effcefde51316550478c250a
APacheco37 <apacheco73@hotmail.com>
2021-04-19 22:33:39 -0300

    Upgrade Angular version from 10 to 11

--- src/app/shared/confimation-dialog/confimation-dialog.component.spec.ts
+++ src/app/shared/confimation-dialog/confimation-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ConfimationDialogComponent } from './confimation-dialog.component';
 
@@ -6,7 +6,7 @@ describe('ConfimationDialogComponent', () => {
   let component: ConfimationDialogComponent;
   let fixture: ComponentFixture<ConfimationDialogComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ConfimationDialogComponent ]
     })
